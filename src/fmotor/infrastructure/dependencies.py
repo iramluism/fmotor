eb0db2df6cf70281fd1b9889b0d4612871701113
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-11-26 00:54:20 -0500

    setup fmotor dependencies

--- src/fmotor/infrastructure/dependencies.py
+++ src/fmotor/infrastructure/dependencies.py
@@ -1,28 +1,52 @@
+""" Fmotor Fmotor Dependencies Module """
+
 from dependency_injector import containers, providers
 
 
 class FMotorContainer(containers.DeclarativeContainer):
+    """ FMotorContainer class """
 
     wiring_config = containers.WiringConfiguration(
         modules=[
             "src.fmotor.ui.app",
             "src.fmotor.ui.view_models",
+            "src.fmotor.ui.components",
+            "src.fmotor.application.queries",
+            "src.fmotor.application.commands",
+            "src.fmotor.domain.services",
+            "src.fmotor.infrastructure.repositories"
         ]
     )
 
     config = providers.Configuration()
 
+    motor_cache = providers.Singleton("src.fmotor.ui.cache.MotorCache")
+
+    estimate_motor_view_model = providers.Factory(
+        "src.fmotor.ui.view_models.EstimateMotorViewModel"
+    )
+
     filter_motor_view_model = providers.Factory(
         "src.fmotor.ui.view_models.FilterMotorViewModel")
 
-    plot_motor_detail_view_model = providers.Factory(
-        "src.fmotor.ui.view_models.GetMotorDetailViewModel")
+    filter_motor_query = providers.Factory(
+        "src.fmotor.application.queries.FilterMotorQuery",
+    )
 
-    calculate_motor_properties_view_model = providers.Factory(
-        "src.fmotor.ui.view_models.CalculateMotorPropertiesViewModel")
+    motor_repository = providers.Factory(
+        "src.fmotor.infrastructure.repositories.MotorRepository")
 
-    estimate_motor_properties_view_model = providers.Factory(
-        "src.fmotor.ui.view_models.EstimateMotorPropertiesViewModel")
+    get_nearest_motor_service = providers.Factory(
+        "src.fmotor.domain.services.GetNearestMotorService")
 
+    motor_validator = providers.Factory(
+        "src.fmotor.domain.validations.MotorValidator")
 
+    get_motor_error_service = providers.Factory(
+        "src.fmotor.domain.services.GetMotorErrorService")
 
+    estimate_motor_command = providers.Factory(
+        "src.fmotor.application.commands.EstimateMotorCommand")
+
+    estimate_motor_service = providers.Factory(
+        "src.fmotor.domain.services.EstimateMotorService")
