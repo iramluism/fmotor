bbafe0518ee07a7fae3e1c92f86c292a88d743bb
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-11-26 00:38:00 -0500

    add Dto interfaces

--- src/seedwork/infrastructure/dtos.py
+++ src/seedwork/infrastructure/dtos.py
@@ -1,5 +1,7 @@
+""" DTO Interfaces Module """
 
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, fields
+from typing import NoReturn, Any, List, Optional
 
 
 @dataclass
@@ -12,8 +14,13 @@ class IDTO:
 		"""
 		return asdict(self)
 
-	def get(self, field):
-		return self.__getattribute__(field)
+	def get(self, field: str, default_value: Optional[Any] = None) -> Any:
+		return self.__getattribute__(field) or default_value
 
-	def set(self, field, value):
-		return self.__setattr__(field, value)
+	def set(self, field: str, value: Any) -> NoReturn:
+		self.__setattr__(field, value)
+
+	@classmethod
+	def fields(cls) -> List[str]:
+		""" Get all fields of the DTO """
+		return [field.name for field in fields(cls)]
