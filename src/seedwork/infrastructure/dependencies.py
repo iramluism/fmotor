e1a47f78114e1d9b24aa9e5311188cdec8a147a4
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-11-26 00:37:25 -0500

    add dependencies to seedwork module

--- src/seedwork/infrastructure/dependencies.py
+++ src/seedwork/infrastructure/dependencies.py
@@ -1,17 +1,22 @@
+""" Containers Interfaces Module """
 
 from dependency_injector import containers, providers
 from src.seedwork.ui.utils import get_running_app
 
+
 class IContainer(containers.DeclarativeContainer):
+	""" IContainer class """
 
 	wiring_config = containers.WiringConfiguration(
 		modules=[
-			"src.seedwork.ui.app"
+			"src.seedwork.ui.app",
+			"src.seedwork.ui.view_models",
+			"src.seedwork.ui.builder"
 		]
 	)
 
+	component_cache = providers.Singleton("src.seedwork.ui.cache.ComponentCache")
 	builder = providers.Singleton("src.seedwork.ui.builder.IBuilder")
-	root_component = providers.Factory("kivymd.uix.widget.MDWidget")
 	ui_theme = providers.Singleton("src.seedwork.ui.themes.IDarkTheme")
 	current_app = providers.Callable(get_running_app)
 
