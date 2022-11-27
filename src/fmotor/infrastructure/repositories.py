b6701a96ab09b3a5a69338256f29bb3a60ec9338
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-11-27 03:21:50 -0500

    get motors value from db

--- src/fmotor/infrastructure/repositories.py
+++ src/fmotor/infrastructure/repositories.py
@@ -5,35 +5,28 @@ from typing import List
 from src.seedwork.infrastructure.repositories import IRepository
 from src.fmotor.domain.entities import MotorEntity
 
-from .utils import parse_filter, eval_filter
+from .mappers import DBMotorMapper
 
 
 class MotorRepository(IRepository):
 	""" MotorRepository class """
 
+	table_name = "nemamotors"
+
 	def save(self):
 		pass
 
 	def all(self):
-		pass
+		return self.filter()
 
 	def filter(self, **filters) -> List[MotorEntity]:
-		motors = []
-		for motor in self.all():
-
-			results = []
-			for field, _filter in filters.items():
-				op, value = parse_filter(_filter)
-				result = eval_filter(motor.get(field), op, value, locals())
-				results.append(result)
-
-			if all(results):
-				motors.append(motor)
-
-		return motors
+		motors = self.db.get_list(self.table_name, filters, as_dict=True)
+		return DBMotorMapper.create_entities(motors)
 
 	def get(self, motor_id):
-		motors = self.filter(id=motor_id)
+		motors = self.db.get_list(
+			self.table_name, {"motor_id": motor_id}, as_dict=True, length=1)
+
 		if motors:
 			return motors[0]
 
