08e986c02e34f85013eb9bfad3bedc5d39092b79
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-11-27 03:15:45 -0500

    Add Sqlite Manager

---
+++ src/seedwork/infrastructure/database.py
@@ -0,0 +1,73 @@
+""" Infrastructure Databases Module """
+
+import sqlite3
+import abc
+
+from typing import Optional
+
+from .utils import build_query
+
+
+class DatabaseManager(metaclass=abc.ABCMeta):
+	""" DatabaseManager class """
+
+	@abc.abstractmethod
+	def execute(self, *args, **kwargs):
+		pass
+
+
+class SQLiteManager(DatabaseManager):
+	""" SQLiteManager class """
+
+	def __init__(self, db_path):
+		self.conn = sqlite3.connect(db_path)
+		self._cur = None
+
+	def close(self):
+		""" close cursor connection """
+		if self._cur:
+			self._cur.close()
+
+	def get_list(self, table, filters: Optional[dict] = None,
+	             fields: Optional[dict] = None, as_dict: bool = False,
+	             length: Optional[int] = None) -> list:
+		""" Get report list
+		:param table: Table name
+		:param filters: conditions
+		:param fields: fields to select
+		:param as_dict: return each report in dict format
+		:param length: maximum reports number
+		"""
+		query = build_query(table, filters, fields)
+		response = self.execute(query)
+
+		if as_dict:
+			result = self._convert_response_to_dict(response)
+		else:
+			result = response.fetchall()
+
+		result = result[:length]
+
+		self.close()
+		return result
+
+	@staticmethod
+	def _convert_response_to_dict(response):
+		""" Convert response to dict """
+		results = []
+		fields_props = response.description
+
+		for row in response.fetchall():
+			dict_result = {}
+			for idx, value in enumerate(row):
+				fieldname = fields_props[idx][0]
+				dict_result[fieldname] = value
+			results.append(dict_result)
+		return results
+
+	def execute(self, query: str):
+		""" Execute sql query """
+		cur = self.conn.cursor()
+		response = cur.execute(query)
+		return response
+
---
+++ src/seedwork/infrastructure/utils.py
@@ -0,0 +1,65 @@
+""" Fmotor Infrastructure Utils Module """
+
+from pypika import Table, Query, Field
+from typing import Union, Iterable, Optional
+
+
+def build_query(table_name: str, filters: Optional[dict] = None,
+                fields: Optional[dict] = None):
+
+	table = Table(table_name)
+
+	if not fields:
+		fields = "*"
+
+	query = Query.from_(table).select(fields)
+
+	if filters:
+		conditions = build_conditions(filters)
+		query = query.where(conditions)
+
+	return str(query)
+
+
+def build_conditions(filters: dict):
+
+	operations_map = {
+		"==": Field.eq,
+		"!=": Field.ne,
+		">=": Field.gte,
+		">": Field.gt,
+		"<=": Field.lte,
+		"<": Field.lt,
+		"not in": Field.notin,
+		"between": Field.between
+	}
+
+	conditions = None
+
+	for fieldname, _filter in filters.items():
+		field = Field(fieldname)
+		op, value = parse_filter(_filter)
+
+		operation = operations_map.get(op)
+		if not operation:
+			raise TypeError("Operator %s not is valid" % op)
+
+		cond = operation(field, value)
+
+		if not conditions:
+			conditions = cond
+		else:
+			conditions &= cond
+
+	return conditions
+
+
+def parse_filter(_filter: Union[str, Iterable]):
+	op = "=="
+	value = _filter
+
+	if isinstance(_filter, (tuple, list)):
+		op = _filter[0]
+		value = _filter[1]
+
+	return op, value
