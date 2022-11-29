3ccf72b5361c85115bb8b5969f69bdc327717a67
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-11-29 12:36:27 -0500

    get motor aggregate from mapper

--- src/fmotor/application/mappers.py
+++ src/fmotor/application/mappers.py
@@ -1,43 +1,73 @@
 """ Fmotor Application Mappers Module """
 
-from src.seedwork.application.mappers import IMapper
+from src.seedwork.domain.mappers import IMapper
 
-from .dtos import MotorDTO
-from src.fmotor.domain.entities import MotorEntity
+from .dtos import MotorDTO, CalculateMotorDTO
+from src.fmotor.domain.entities import MotorEntity, MotorMeasurement
+from src.fmotor.domain.aggregates import MotorAggregate
+from .utils import encrypt_motor_id, decrypt_motor_id
 
 
 class MotorMapper(IMapper):
 	""" MotorMapper class """
 
 	@classmethod
-	def create_entity(cls, motor_dto: MotorDTO) -> MotorEntity:
+	def create_aggregate(cls, motor_dto: MotorDTO) -> MotorAggregate:
 		""" Create Motor entity from Motor DTO """
 
-		motor_entity = cls.map(
-			from_obj=motor_dto,
-			to_obj=MotorEntity,
-			mapping={
-				"v_nom": "voltage",
-			},
-			missing_values={
-				"hp_nom": motor_dto.kw * 1.34,
-				"eff_fl": 1,
-				"pf_fl": 1
-			}
+		missing_values = {}
+		if motor_dto.kw and not motor_dto.hp_nom:
+			missing_values["hp_nom"] = motor_dto.kw * 1.34
+		elif motor_dto.hp_nom and not motor_dto.kw:
+			missing_values["kw"] = motor_dto.hp_nom / 1.34
+		if motor_dto.id:
+			missing_values["motor_id"] = decrypt_motor_id(motor_dto.id)
+
+		motor_entity = cls.map_objs(
+			_from=motor_dto,
+			_to=MotorAggregate,
+			missing_values=missing_values
 		)
 
 		return motor_entity
 
 	@classmethod
-	def create_dto(cls, motor_entity: MotorEntity):
+	def create_dto(cls, motor_aggr: MotorAggregate) -> MotorDTO:
 		""" Create Motor DTO from Motor entity """
 
-		motor_dto = cls.map(
-			from_obj=motor_entity,
-			to_obj=MotorDTO,
+		motor_dto = cls.map_objs(
+			_from=motor_aggr,
+			_to=MotorDTO,
 			mapping={
-				"voltage": "v_nom",
-			}
+				"type": "motor_type",
+			},
+			default_values={
+				"id": encrypt_motor_id(motor_aggr.motor_id)
+			},
+			excluded_fields=["manufacturer_id", "motor_type_id"]
 		)
 
 		return motor_dto
+
+
+class MotorMeasurementMapper(IMapper):
+
+	@classmethod
+	def create_entity(cls, dto: CalculateMotorDTO):
+
+		motor = MotorMapper.create_aggregate(dto.motor)
+
+		entity = cls.map_objs(
+			_from=dto, _to=MotorMeasurement,
+			default_values={
+				"motor": motor
+			}
+		)
+
+		return entity
+
+	@classmethod
+	def create_dto(cls, entity):
+
+		dto = cls.map_objs(_from=entity, _to=CalculateMotorDTO)
+		return dto
