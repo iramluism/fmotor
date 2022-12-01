68fe31162939489fae473d1d73e3225fc47b7475
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-12-01 13:08:37 -0500

    add translation functionalities to utils module

--- src/seedwork/ui/utils.py
+++ src/seedwork/ui/utils.py
@@ -2,6 +2,7 @@
 
 from .cache import ComponentCache
 from .app import IApp
+from .translations import Translator, Language
 
 
 def get_running_app():
@@ -14,3 +15,16 @@ def get_component(_id):
 	cache = ComponentCache()
 	component = cache.get(_id)
 	return component
+
+
+def _(text, lang=None, *, context=None):
+	""" Translate text to lang, otherwise return the same text
+	:param text: text to translate
+	:param lang: (es, en, etc ..), by default take the system language
+	:param context: translate text according to context
+	"""
+
+	translator = Translator(Language.EN)
+	translated_text = translator.translate(text, lang, context)
+
+	return translated_text
