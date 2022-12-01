be800c0e307b73668fa82a51c669199acf5c20ed
Iram Luis Morales Santos <iram.morales@generalsoftwareinc.com>
2022-12-01 13:03:04 -0500

    refactor convert function

--- src/fmotor/ui/utils.py
+++ src/fmotor/ui/utils.py
@@ -8,10 +8,15 @@ def convert(value, to, default=None):
 	:param default: default value if raise a value error on converting
 	"""
 
-	try:
-		return to(value)
-	except ValueError:
-		return default or to()
+	result = default
+
+	if value:
+		try:
+			result = to(value)
+		except ValueError:
+			result = default or to()
+
+	return result
 
 
 def prepare_motor_values(motor: dict) -> dict:
